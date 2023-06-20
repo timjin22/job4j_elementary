5f2b3e0ffc9fa1fe845b6ce9ee569017b568e49f
timjin22 <tim22reev@gmail.com>
2023-06-20 21:58:08 +0300

    my variables

---
+++ src/main/java/ru/job4j/calculate/MyVars.java
@@ -0,0 +1,22 @@
+package ru.job4j.calculate;
+
+public class MyVars {
+    public static void main(String[] args) {
+        byte age = 25;
+        System.out.println(age);
+        char sex = 'Ж';
+        System.out.println(sex);
+        float height = 1.75F;
+        System.out.println(height);
+        short numberOfBricks = 30000;
+        System.out.println(numberOfBricks);
+        double numberOfBacteria = 3.9E13;
+        System.out.println(numberOfBacteria);
+        double distance = 87523222.76;
+        System.out.println(distance);
+        boolean jobLicense = true;
+        System.out.println(jobLicense);
+        long fileSize = 253476704;
+        System.out.println(fileSize);
+    }
+}
