a957194dfaa1aca255f3fd4743f3aa870fbad199
timjin22 <tim22reev@gmail.com>
2023-06-20 22:50:11 +0300

    new variable

---
+++ src/main/java/ru/job4j/calculate/NewVar.java
@@ -0,0 +1,10 @@
+package ru.job4j.calculate;
+
+public class NewVar {
+    public static void main(String[] args) {
+        int size = 10;
+        size = 100;
+        size = size - 5;
+        System.out.println(size);
+    }
+}
