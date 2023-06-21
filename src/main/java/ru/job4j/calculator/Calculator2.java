025dae2bb95d6b15913155f65ed0becfa3ce6f5b
timjin22 <tim22reev@gmail.com>
2023-06-21 20:58:29 +0300

    Calculator2

--- src/main/java/ru/job4j/calculator/Calculator2.java
+++ src/main/java/ru/job4j/calculator/Calculator2.java
@@ -9,5 +9,8 @@ public class Calculator2 {
     public  static void main(String[] args) {
         Calculator2.plus(1, 2);
         Calculator2.plus(10, 11);
+        Calculator2.plus(100, 500);
+        Calculator2.plus(4, 2);
+        Calculator2.plus(3, 5);
     }
 }
