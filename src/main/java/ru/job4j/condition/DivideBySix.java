cd1589db0b654b07b08c86518d4a7aae51d2e3da
timjin22 <tim22reev@gmail.com>
2023-07-01 23:38:48 +0300

    4.4. Вложенный оператор if else. Запутанный код [#190]

---
+++ src/main/java/ru/job4j/condition/DivideBySix.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+public class DivideBySix {
+    public static String checkNumber(int number) {
+        String rsl;
+        if (number % 3 == 0 && number % 2 == 0) {
+            rsl = "The number divides by 6.";
+        } else if (number % 3 == 0) {
+            rsl = "The number divides by 3, but it isn't the even number.";
+        } else if (number % 2 == 0) {
+            rsl = "The number doesn't divide by 3, but it is the even number.";
+        } else {
+            rsl = "The number doesn't divide by 3 and it isn't the even number.";
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(DivideBySix.checkNumber(8));
+        System.out.println(DivideBySix.checkNumber(12));
+        System.out.println(DivideBySix.checkNumber(9));
+        System.out.println(DivideBySix.checkNumber(7));
+    }
+}
