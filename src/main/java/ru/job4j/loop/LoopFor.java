ca68ff39a15f802d1925b6334aa8e1d758937068
timjin22 <132806552+timjin22@users.noreply.github.com>
2023-07-02 12:53:11 +0300

    Delete src/main/java/ru/job4j/loop directory

--- src/main/java/ru/job4j/loop/LoopFor.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.loop;
-
-public class LoopFor {
-    public static void main(String[] args) {
-        for (int i = 0; i <= 5; i++) {
-            System.out.println(i);
-        }
-    }
-}
