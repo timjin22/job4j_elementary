f0ac28d50226e24a655a6808bc1da143416458e2
timjin22 <tim22reev@gmail.com>
2023-07-02 12:36:40 +0300

    4.6.2. Switch - matcher [#334071]

---
+++ src/main/java/ru/job4j/loop/LoopFor.java
@@ -0,0 +1,9 @@
+package ru.job4j.loop;
+
+public class LoopFor {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
