cf455a6940f914c104329c3c7d773983a6bc4842
timjin22 <tim22reev@gmail.com>
2023-07-04 14:26:35 +0300

    5.7. Ипотека [#156315]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -2,10 +2,9 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
-        int year = 1;
-        double debt = amount + amount * (percent / 100);
-        while (debt - salary > 0) {
-            debt -= salary;
+        int year = 0;
+        while (amount > 0) {
+            amount += amount * (percent / 100) - salary;
             year++;
         }
         return year;
